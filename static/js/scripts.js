d039e852b65ccef87613c83cda6fb14eeff09bed
Clupai8o0 <clupaio4@gmail.com>
2022-07-30 10:51:40 +0300

    doing image transitions even on different pages

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,33 +1,41 @@
-const headers = document.querySelectorAll("h2, h3");
-const imageHolders = document.querySelectorAll("div.image");
+// to run the image animation in every page
+const runScripts = () => {
+	const headers = document.querySelectorAll("h2, h3");
+	const imageHolders = document.querySelectorAll("div.image");
 
-const observer = new IntersectionObserver(
-	(entries) => {
-		entries.forEach((entry) => {
-			if (entry.intersectionRatio >= 0.1) {
-				entry.target.classList.add("in-view");
-			} else {
-				entry.target.classList.remove("in-view");
-			}
-		});
-	},
-	{
-		threshold: [0, 0.1, 1],
-	}
-);
+	const observer = new IntersectionObserver(
+		(entries) => {
+			entries.forEach((entry) => {
+				if (entry.intersectionRatio >= 0.1) {
+					entry.target.classList.add("in-view");
+				} else {
+					entry.target.classList.remove("in-view");
+				}
+			});
+		},
+		{
+			threshold: [0, 0.1, 1],
+		}
+	);
 
-headers.forEach((header) => {
-	observer.observe(header);
-});
-imageHolders.forEach((holder) => {
-	observer.observe(holder);
-});
+	headers.forEach((header) => {
+		observer.observe(header);
+	});
+	imageHolders.forEach((holder) => {
+		observer.observe(holder);
+	});
+};
+runScripts();
 
 //* Barba
 barba.init({
 	transitions: [
 		{
 			name: "switch",
+			leave({ current, next, trigger }) {},
+			enter({ current, next, trigger }) {
+				runScripts();
+			},
 		},
 	],
 	views: [],
